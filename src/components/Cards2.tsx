ef2c28782a472f3129199fe3f95a866a4d6af34d
pupiesa <puppup.2547.2016@gmail.com>
2025-10-07 00:50:16 -0700

    feat: update Cards2 component layout and enhance alert styling

--- src/components/Cards2.tsx
+++ src/components/Cards2.tsx
@@ -8,7 +8,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-import { AlertCircleIcon } from "lucide-react";
+import { AlertCircleIcon, CircleSmall } from "lucide-react";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 const Cards = () => {
@@ -16,19 +16,34 @@ const Cards = () => {
     <Card className="h-full">
       <CardHeader>
         <CardTitle>Recent Status Changes</CardTitle>
-        <div className="flex flex-row justify-between">
-          <CardContent>
-            Laptop - Room 201
-            <CardDescription>Card Footer</CardDescription>
-          </CardContent>
-          <div className="w-30% max-w-lg">
-            <Alert variant="destructive">
-              <AlertCircleIcon />
-              <AlertTitle>Please </AlertTitle>
-              <AlertDescription></AlertDescription>
-            </Alert>
-          </div>
-        </div>
+        <ul className="flex flex-col gap-2">
+          <li className="flex flex-row justify-between">
+            <CardContent>
+              Laptop - Room 201
+              <CardDescription>Card Footer</CardDescription>
+            </CardContent>
+            <div className="w-30% max-w-lg">
+              <Alert className="rounded-full">
+                <CircleSmall className="fill-foreground rounded-full" />
+                <AlertTitle>Please </AlertTitle>
+                <AlertDescription></AlertDescription>
+              </Alert>
+            </div>
+          </li>
+          <li className="flex flex-row justify-between">
+            <CardContent>
+              Laptop - Room 201
+              <CardDescription>Card Footer</CardDescription>
+            </CardContent>
+            <div className="w-30% max-w-lg">
+              <Alert className="rounded-full">
+                <CircleSmall className="fill-foreground rounded-full" />
+                <AlertTitle>Please </AlertTitle>
+                <AlertDescription></AlertDescription>
+              </Alert>
+            </div>
+          </li>
+        </ul>
         {/* <CardAction>
           <Wrench className="text-blue-400" />
         </CardAction> */}
